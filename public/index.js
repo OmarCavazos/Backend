3c0a32612da01d1b5eead7cf081a266b9677ef80
Omar <omar.cavazos92@gmail.com>
2023-01-20 17:29:04 -0600

    usserMessage con websocket

--- public/index.js
+++ public/index.js
@@ -1,6 +1,43 @@
 const socket = io();
+const form = document.querySelector("#form")
+const user=document.querySelector("#user")
+const userMessage = document.querySelector("#userMessage")
+const chat = document.querySelector("#chat")
 
-socket.on("saludo", (data) => {
-    console.log(data);
-    socket.emit("respuesta", "hola, soy el cliente")
+let dataGet = null
+
+const addMessage =(data) => {
+    const messages = data.map((m,i) =>{
+        if (m.user === dataGet[i-1]?.user) {
+            return`
+            <dd>${m.message}</dd>`
+        } else {
+            return `<dt>${m.user}</dt>
+            <dt>${m.message}</dt>`
+            }
+        }
+        ).join("")
+    chat.innerHTML = messages
+}
+
+
+socket.on("messages", (data) => {
+    dataGet = data    
+    addMessage(data)
 })
+
+form.onsubmit = (e) => {
+    e.preventDefault()
+    const isUser = dataGet.some(u=>u.user===user.value)
+        if (isUser) {
+            alert("ese usuario ya existe")
+        } else {
+            const message = {
+                user:user.value,
+                message: userMessage.value
+            }
+            socket.emit("newMessage", message)
+            userMessage.value = ""
+            user.style.display = "none"
+        }
+}
--- server.js
+++ server.js
@@ -13,22 +13,24 @@ const app = express()
 const httpServer = createServer(app)
 const io= new Server(httpServer)
 
+const ip = "192.168.0.4"
 
 app.use(express.static("public"))
-
 app.get("/", (req, res) =>{
     res.sendFile(`${__dirname}/index.html`)
 })
 
+let messages = []
+
 io.on("connection", (socket)=>{
     console.log("hola", socket.id);
-    socket.emit("saludo", "hola como estas, soy el servidor")
-    socket.on("respuesta", data =>{
-        console.log(data);
+    socket.emit("messages", messages )
+    socket.on("newMessage", data => {
+        messages.push(data)
+        io.sockets.emit("messages", messages)
     })
 })
 
 
-
-
-httpServer.listen(3000, ()=> console.log("servidor OK"))
+httpServer.listen(3000,"192.168.0.4", ()=>
+console.log("servidor OK"))
