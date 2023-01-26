fb53799593e13c03837f9c7e55e7f87b5dde70e1
Omar <omar.cavazos92@gmail.com>
2023-01-26 16:07:28 -0600

    terminado

--- api/server.js
+++ api/server.js
@@ -10,9 +10,37 @@ const io = new Server(httpServer, {
     }
 });
 
+const rooms = {
+    room1: [{
+        user: "yo",
+        message: "sala 1"
+    }],
+    room2: [{
+        user: "tu",
+        message: "sala 2"
+    }],
+    room3: [{
+        user: "el",
+        message: "sala 3"
+    }],
+}
+
 io.on("connection", (socket) => {
-    console.log("hola", socket.id);
+    socket.on("room", room => {
+        socket.join(room);
+        io.to(rooms).emit("messages", rooms[room]);
+    });
+
+    socket.on("newMessage", newMessage => {
+        const [messages, room] = newMessage
+        rooms[room].push(
+            messages
+        )
+        socket.join(room)
+        io.to(room).emit("messages", rooms[room])
+    })
 });
+
 
 httpServer.listen(3000,()=>{
     console.log("Servidor jalando");
